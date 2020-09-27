39d9607f7bff51e3f8045dfae5c250902224fb9c
Kesh-man <luis.contatoweb@gmail.com>
2020-09-27 18:28:00 -0300

    new function

--- hashmap.c
+++ hashmap.c
@@ -23,6 +23,9 @@ unsigned long elf_hash(const char *s) {
 struct item{
   char* key;
   int value;
+  struct item *next;
+  struct item *prev;
+} item;
 };
 //HashTable | size
 struct hashmap{
@@ -32,34 +35,49 @@ struct hashmap{
 };
 
 hashmap *hashmap_create(int capacity){
-  hashmap* map = (hashmap*) malloc (sizeof(hashmap));
-  map->capacity = capacity;
-  map->count = 0;
-  map->items = (item**) calloc (map->capacity, sizeof(item*));
-  for (int i=0; i<map->capacity; i++)
+  if(capacity<1){
+    return NULL;
+  }else{  
+    hashmap* map = (hashmap*) malloc (sizeof(hashmap));
+    map->capacity = capacity;
+    map->count = 0;
+    map->items = (item**) calloc (map->capacity, sizeof(item*));
+    for (int i=0; i<map->capacity; i++){
       map->items[i] = NULL;
-
-  return map;
-
+    }
+    return map;
+    }
 }
 
 void hashmap_set(hashmap *map, const char *key, int value){
-    
     //criar item na tabela
     item* new_item = (item*) malloc (sizeof(item));
     new_item->key = (char*) malloc(strlen(key) +1);
     new_item->value = (int) malloc(value +1);
-
-    strcpy(new_item->key,key);
-    new_item->value = value;
-    
-    //criar hash do item
-    int hash = elf_hash(key);
-
-    //adicionando item
+ 
     item* item_actual = map->items[hash];
-    map->items[hash] = new_item;
-    map->count++;
-
+     
+    if (item_actual == NULL) {
+        //key nao existe
+        if (map->count == map->capacity) {
+            //tabela cheia
+            printf("Tabela cheia\n");
+            return;
+        }
+         
+        //adicionando item
+        map->items[hash] = new_item; 
+        map->count++;
+    }else {
+        //atualizar valor
+        if (strcmp(item_actual->key, key) == 0) {
+            strcpy(map->items[hash]->value, value);
+            return;
+        }else{
+            //colisão tratar mais tarde
+            colision(map, item);
+            return;
+        }
+    }
 }
 
